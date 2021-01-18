11d569ac4c565900afade2532c3470bb3a425c00
Mario Rabelo <mutrano@gmail.com>
2021-01-18 18:04:07 -0300

    add Service layer

--- src/main/java/com/mutra/springb/repositories/UserRepository.java
+++ src/main/java/com/mutra/springb/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.mutra.springb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mutra.springb.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
---
+++ src/main/java/com/mutra/springb/services/UserService.java
@@ -0,0 +1,25 @@
+package com.mutra.springb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mutra.springb.entities.User;
+import com.mutra.springb.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
