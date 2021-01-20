b4880e59211cd2bd04a8b74f35e87bf0af3e3fbe
Mário Rabelo <mutrano@gmail.com>
2021-01-20 15:47:20 -0300

    Payment and it's one-to-one association with Order

--- src/main/java/com/mutra/springb/config/TestConfig.java
+++ src/main/java/com/mutra/springb/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.mutra.springb.config;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.mutra.springb.entities.Category;
 import com.mutra.springb.entities.Order;
 import com.mutra.springb.entities.OrderItem;
+import com.mutra.springb.entities.Payment;
 import com.mutra.springb.entities.Product;
 import com.mutra.springb.entities.User;
 import com.mutra.springb.entities.enums.OrderStatus;
@@ -81,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
