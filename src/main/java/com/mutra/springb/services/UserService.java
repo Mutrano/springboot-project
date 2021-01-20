35102e7c9c55b9323c04099280e3c4022444faa4
Mário Rabelo <mutrano@gmail.com>
2021-01-20 20:33:56 -0300

    add ExceptionHandling on findById in case the resource is not found

--- src/main/java/com/mutra/springb/services/UserService.java
+++ src/main/java/com/mutra/springb/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mutra.springb.entities.User;
 import com.mutra.springb.repositories.UserRepository;
+import com.mutra.springb.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow( () -> new ResourceNotFoundException(id)  );
 	}
 
 	public User insert(User obj) {
