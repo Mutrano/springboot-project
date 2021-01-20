d52df4f06f9c2b48d5e4a9a07343b82043df03d3
Mário Rabelo <mutrano@gmail.com>
2021-01-20 13:20:49 -0300

    add OrderItems and it's test seeds

--- src/main/java/com/mutra/springb/entities/Product.java
+++ src/main/java/com/mutra/springb/entities/Product.java
@@ -22,8 +22,8 @@ public class Product implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
 	private Long id;
+	
 	private String name;
 	private String description;
 	private Double price;
